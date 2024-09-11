60c6e21d43c748ecabeacbe7a5349840991109ec
Karoly1996 <futyi1996@hotmail.com>
2024-09-10 20:54:19 -0400

    fixed signup process

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,3 +1,6 @@
+import { User } from "../models/user.js";
+import bcryptjs from "bcryptjs"
+
 export async function signup(req, res) {
   try {
     const {email,password,username} = req.body;
@@ -5,13 +8,61 @@ export async function signup(req, res) {
     if(!email || !password || !username) {
       return res.status(400).json({success:false, messsage: "All fields are required" })
     }
-  } catch (error) {
 
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(emailRegex.test(email)){
+      return res.status(400).json({success:false,message:"Invalid email"})
+    }
+
+    if(password.length < 6) {
+      return res.status(400).json({success:false,message:"Password must be atleast 6 characters"})
+    }
+
+    const existingUserByEmail = await User.findOne({email:email})
+    if(existingUserByEmail) {
+      return res.status(400).json({success:false,message:"Email already exists"})
+    }
+
+    const existingUserByUsername = await User.findOne({username:username})
+    if(existingUserByUsername) {
+      return res.status(400).json({success:false,message:"Username already exists"})
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const PROFILE_PICS = ["/avatar1.png", "/avatar2.png","/avatar3.png"];
+    const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
+    
+    const newUser = new User({
+      email:email,
+      password:hashedPassword,
+      username:username,
+      image:image
+    })
+
+    await newUser.save();
+
+    // remove password from response
+    res.status(201).json({success: true, user: {
+      ...newUser._doc,
+      password:""
+    }})
+  } catch (error) {
+    console.log("Error in signup controller", error.message)
+    res.status(500).json({success:false,message:"Internal server error"})
   }
 }
+
+
+
 export async function login(req, res) {
   res.send("Login Route")
 }
+
+
+
 export async function logout(req, res) {
   res.send("Logout Route")
 }
